858e9c4472c890afadf4f124216156e98e8e7234
Travis <travisseal@gmail.com>
2017-06-11 23:44:38 -0700

    added comments

--- ParsingXMLDocumentTree.py
+++ ParsingXMLDocumentTree.py
@@ -4,6 +4,7 @@ import pprint
 tree = et.parse('E:\Projects\Python\Intro DataScience\data\south_carolina.osm')
 root = tree.getroot()
 
+#print the tag attribute of each child element.
 print("\nChildren of root : ")
 for child in root:
     print (child.tag)
